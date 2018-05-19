dbed5849670af3d87b9265e53ecdc9937168dc10
Dragomir Turcanu <dragomirt22@gmail.com>
2018-05-19 05:20:43 +0300

    Creates a readme.json file in the lists folder by default

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const signale = require('signale');
 const chalk = require('chalk');
 const figlet = require('figlet');
 const clear = require('clear');
@@ -12,26 +13,36 @@ try{
 		if(error) throw error;
 		appConfig = require('./config.json');
 		initDefaultConfig();
-		console.log('Hi!')
 	});
 }
+
+initReadme();
 
 // Initiate config JSON if it's empty
 function initDefaultConfig(){
 	if(appConfig === undefined){
 		appConfig.defaultPath = "./";
-		appConfig.listsPath = "./lists";
+		appConfig.listsPath = "./lists/";
 		appConfig.listsType = "json"
 		fs.writeFileSync('./config.json', JSON.stringify(appConfig))
 	}
 }
 
+// Init lists folder
+function initReadme(){
+	try{
+		check = fs.readdirSync(`${appConfig.listsPath}readme.json`);
+	}catch(err){
+		signale.fatal(new Error(err));
+		fs.writeFile(`${appConfig.listsPath}readme.json`, `{}` , function(err){
+			if(err) signale.fatal(`\n${err}`);
+		});
+	}
+}
+
 clear();
-console.log(
-	chalk.green(
-		figlet.textSync('To Do', {horizontalLayout: 'full'})
-	)
-);
+console.log(figlet.textSync('To Do', {horizontalLayout: 'full'}));
+signale.success("Made by: Dragomir Turcanu");
 
 function askDetails(){
 	const argv = require('minimist')(process.argv.slice(2));
@@ -39,17 +50,45 @@ function askDetails(){
 	var questions = [
 		{
 		  type: 'list',
-		  name: 'size',
-		  message: 'What size do you need?',
-		  choices: ['kek', 'lul', '123'],
+		  name: 'taskFileOperation',
+		  message: 'What would you like to do with a task list file',
+		  choices: ['open', 'create', 'delete'],
 		  filter: function(val) {
 			return val.toLowerCase();
 		  }
 		}
 	  ];
 
-	inquirer.prompt(questions).then(answers => {console.log(answers)})
-    //return inquirer.prompt(questions);
+	inquirer.prompt(questions).then(answers => {
+		if(answers.taskFileOperation === 'open'){
+			var qst = [
+				{
+					type: 'list',
+					name: 'openFileName',
+					message: 'Which file would you like to open',
+					choices: getTskFileList(),
+					filter: function(val) {
+					return val.toLowerCase();
+					}
+				}
+			];
+		}else if(answers.taskFileOperation === 'create'){
+
+		}else if(answers.taskFileOperation === 'delete'){
+
+		}
+
+		inquirer.prompt(qst).then(answ=> {
+			if(answ.openFileName){
+				let taskData = require(`${appConfig.listsPath}${answ.openFileName}`);
+				
+			}
+		});
+	});
+}
+
+function getTskFileList(){
+	return fs.readdirSync(appConfig.listsPath)
 }
 
 askDetails();
