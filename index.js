b0add2b6825e27ded5f2bca6a606a9c84653909a
Dragomir Turcanu <dragomirt22@gmail.com>
2018-05-19 03:34:47 +0300

    testing

---
+++ index.js
@@ -0,0 +1,52 @@
+const chalk = require('chalk');
+const figlet = require('figlet');
+const clear = require('clear');
+const inquirer = require('inquirer');
+const fs = require('fs');
+let appConfig;
+
+try{
+	appConfig = require('./config.json');
+}catch(err){
+	fs.writeFile('config.json', `{}`, function(error){
+		if(error) throw error;
+		appConfig = require('./config.json');
+		console.log('Hi!')
+	});
+}
+
+// Initiate config JSON if it's empty
+if(appConfig.defaultPath === undefined){
+	appConfig.defaultPath = "./";
+	appConfig.listsPath = "./lists";
+	appConfig.listsType = "json"
+	fs.writeFileSync('./config.json', JSON.stringify(appConfig))
+}
+
+clear();
+console.log(
+	chalk.green(
+		figlet.textSync('To Do', {horizontalLayout: 'full'})
+	)
+);
+
+function askDetails(){
+	const argv = require('minimist')(process.argv.slice(2));
+
+	var questions = [
+		{
+		  type: 'list',
+		  name: 'size',
+		  message: 'What size do you need?',
+		  choices: ['kek', 'lul', '123'],
+		  filter: function(val) {
+			return val.toLowerCase();
+		  }
+		}
+	  ];
+
+	inquirer.prompt(questions).then(answers => {console.log(answers)})
+    //return inquirer.prompt(questions);
+}
+
+askDetails();
